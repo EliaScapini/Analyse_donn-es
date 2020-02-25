36a6d802a9a134a5c9e558cd8430626d5c0cfb6a
EliaScapini <58431627+EliaScapini@users.noreply.github.com>
2020-02-25 14:38:04 +0100

    Essay

--- extrait_donnees.m
+++ extrait_donnees.m
@@ -5,3 +5,4 @@ close all;
 fid=fopen(nom_fic,'r');
 %Lire les valeurs de puissance de la phase 2
 donnees=textscan(fid,'%*s %s %*f %*f %*f %*f %*f %*f %d %d %d %*d %*d %*d %*f %*f %*f %d','Headerlines',1,'Delimiter',';');
+%jhafjsdh
