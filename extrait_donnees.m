cd048ff5df86896b33569188eea6e1165c3e56e7
EliaScapini <58431627+EliaScapini@users.noreply.github.com>
2020-02-28 15:42:25 +0100

    modifications

--- extrait_donnees.m
+++
@@ -1,8 +0,0 @@
-function donnees=extrait_donnees(nom_fic)
-clear all;
-close all;
-%Open the file
-fid=fopen(nom_fic,'r');
-%Lire les valeurs de puissance de la phase 2
-donnees=textscan(fid,'%*s %s %*f %*f %*f %*f %*f %*f %d %d %d %*d %*d %*d %*f %*f %*f %d','Headerlines',1,'Delimiter',';');
-%jhafjsdh
